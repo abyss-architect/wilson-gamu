68f17cd402b8b50fd9499b447ffac3c0d4d89dcb
wjvm0203 <32793067-wjvm0203@users.noreply.replit.com>
2024-03-22 18:36:16 +0000

    Weapon no longer abstract ; Created Sword Class ; Beginning implementation for attack-speed and for attack-range for weapons.
    1) Since we need to be able to instantiate the class Weapons for polymorphism, opted to make it into a regular class instead of abstract.
    
    2) Began implementation for the sword-type weapons in the game.
    
    3) Added the attackSpeed & attackRange variables for the Weapon class:
    - attackSpeed will determine who attacks first in battle, as well as how fast they're able to swing. once the game is more developed, attackSpeed will hold how many seconds it would take for that specific weapon to do continuous attacks. So if a weapon that has a "1" attackSpeed is going against a weapon that has a "3" attackSpeed, the first weapon should be able to swing three times before the second weapon swings once.
    - attackRange will determine how far an attack is able to go in the cardinal directions. A weapon that has a "0" attackRange can only attack tiles that are next to where the entity is, and a weapon with "3" attack range can hit targets 3 spaces away.

---
+++ src/main/java/Sword.java
@@ -0,0 +1,13 @@
+/*
+  Created by: Wilson J. Villegas M.
+  Last modified: 03/22/2024.
+
+  Sword class that will be used for all the sword-type weapons, which there will be 3 types of: 
+  Dagger,
+  Short sword,
+  Long sword.
+*/
+
+public class Sword extends Weapon {
+  
+}
