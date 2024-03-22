8351572e6e80361fe48f74c2c9b5eafbaac97dd9
wjvm0203 <32793067-wjvm0203@users.noreply.replit.com>
2024-03-22 15:00:38 +0000

    Developed two new classes in replacement of "HealingPotion" and "ManaPotion." The new classes, "Healing" and "Mana" will be used for any items (i.e potions, foods, etc) and/or skills that are able to increase the player's health or the player's mana value.

--- src/main/java/HealingPotion.java
+++
@@ -1,8 +0,0 @@
-/*
-Class for the healing potions. It either increases the player's hp by 10 (normal healing potions), or it increases it by a specified amount.
-*/
-
-public class HealingPotion implements Usable {
-	public void Use(Entity entity) { entity.setHP(entity.getHP() + 10); }
-  public void Use(Entity entity, int amount) { entity.setHP(entity.getHP() + amount); }
-}
---
+++ src/main/java/Mana.java
@@ -0,0 +1,13 @@
+/*
+Class for any mana-increasing act. It either increases the player's mp by 10 (normal mana potions), or it increases it by a specified amount. There might be some skills that can also use this class to increase the player's mana bar. 
+*/
+
+public class Mana extends Item implements Usable {
+	public void Use(Entity entity) {
+		entity.setMP(entity.getMP() + 10);
+	}
+  public void Use(Entity entity, int amount) {
+    entity.setMP(entity.getMP() + amount);
+  }
+  
+}
