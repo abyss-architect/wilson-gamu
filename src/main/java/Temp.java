344436c11c5888689c6447c932ab5e646730c003
AbyssArchitect <abyssarchitectofficial@gmail.com>
2024-04-17 23:07:07 -0400

    Removed Temp file

--- src/main/java/Temp.java
+++
@@ -1,9 +0,0 @@
-public class Temp {
-    public static void Test() {
-        System.out.println("Lulu is the best");
-        int a = 1;
-        int x = 69;
-        String s = "okay bruh";
-        System.out.println("HLISJFLJS" + s);
-    }
-}
