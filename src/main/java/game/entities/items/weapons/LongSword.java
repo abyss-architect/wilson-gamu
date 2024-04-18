668beb936b6f3f60bfc0d1e62a4ec3f41ad84da5
Wilson Villegas <wjvm0203@gmail.com>
2024-04-17 22:51:08 -0400

    Test

--- src/main/java/game/entities/items/weapons/LongSword.java
+++ src/main/java/game/entities/items/weapons/LongSword.java
@@ -1,3 +1,17 @@
 package game.entities.items.weapons;
 
-public class LongSword extends CloseRange { }
+import game.entities.Entity;
+
+public class LongSword extends CloseRange {
+
+    public LongSword(int damage, int range, int speed) {
+        this.setItem("Dagger", "Just a regular dagger.");
+        this.setWeaponDamage(damage);
+        this.setAttackRange(range);
+        this.setAttackSpeed(speed);
+    }
+
+    public void heavySwing(Entity target) {
+        this.setWeaponStats(getWeaponDamage() + 5, getAttackSpeed() - 2, getAttackRange() + 1);
+    }
+}
